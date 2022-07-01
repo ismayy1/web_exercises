e98521154d12c85930492f73817a3fb78c0bbcc8
Ismayyl Atagarryyev <73302785+tkmgala@users.noreply.github.com>
2022-07-01 16:03:19 +0300

    Add files via upload

---
+++ survey-form/code.js
@@ -0,0 +1,10 @@
+//
+// $(document).ready(function(){
+//   var typed = new Typed(".typing", {
+//     strings: ["Thanks for considering to use our survay!", "Great!"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+//   });
+//
+// });
